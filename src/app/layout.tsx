cd3ddf659741724a3ed1f4f23c7ad63ee4552899
Genesis Diaz <genesis.diaz@jmc.edu.ph>
2026-01-10 03:32:54 +0000

    predict analytics for a starter company there should be have a sign for

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from '@/lib/utils';
 export const metadata: Metadata = {
   title: 'DavaoCycle',
   description: 'E-Bike Sharing and Fleet Management',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
