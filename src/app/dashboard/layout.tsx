92cd1c4760a34eafab4a7be70544805426c7502f
Genesis Diaz <genesis.diaz@jmc.edu.ph>
2026-01-12 10:39:44 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import React, 'useEffect', useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRouter } from 'next/navigation';
 import { auth, firestore } from '@/firebase/config';
